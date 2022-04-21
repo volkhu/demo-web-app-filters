8672ae92f156afb1342cd34bda86a5d7bec190dd
volkhu <104153914+volkhu@users.noreply.github.com>
2022-04-21 19:34:09 +0300

    Initialize backend code

---
+++ filters-backend/src/test/java/demo/web/app/filtersbackend/FiltersApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.web.app.filtersbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiltersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
