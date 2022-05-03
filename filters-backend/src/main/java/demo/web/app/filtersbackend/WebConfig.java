68bc70cd8dbf05814adecb0eeaac9a33d8a32620
volkhu <104153914+volkhu@users.noreply.github.com>
2022-05-03 13:45:41 +0300

    Add CORS handling to back-end

---
+++ filters-backend/src/main/java/demo/web/app/filtersbackend/WebConfig.java
@@ -0,0 +1,20 @@
+package demo.web.app.filtersbackend;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${filters.cors.allowed-origins}")
+    private String corsAllowedOrigins;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins(corsAllowedOrigins.split(","));
+    }
+}
