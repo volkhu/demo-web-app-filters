756409fa578067cb982d10e5723a9226e2f179f6
volkhu <104153914+volkhu@users.noreply.github.com>
2022-05-16 13:46:39 +0300

    Fix static content not being served

--- filters-backend/src/main/java/demo/web/app/filtersbackend/config/WebConfig.java
+++ filters-backend/src/main/java/demo/web/app/filtersbackend/config/WebConfig.java
@@ -3,11 +3,9 @@ package demo.web.app.filtersbackend.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Value("${filters.cors.allowed-origins}")
